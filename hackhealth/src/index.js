28bac2d6676fa7693b931f66c5f6c0f5225bd280
shiv-sundar <shivkarthi.sundar@gmail.com>
2021-02-13 05:37:47 -0600

    how to call firebase data after it is set up

--- hackhealth/src/index.js
+++ hackhealth/src/index.js
@@ -2,6 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import firebase from "firebase/app";
+import "firebase/database";
+const keys = require('./keys.json');
+
+const firebaseConfig = {
+  apiKey: keys.apiKey,
+  authDomain: keys.authDomain,
+  projectId: keys.projectId,
+  storageBucket: keys.storageBucket,
+  messagingSenderId: keys.messagingSenderId,
+  appId: keys.appId
+};
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
     <App />,
