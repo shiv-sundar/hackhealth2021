e5802a3805b43cd72d62159ca82b1d614220460a
shiv-sundar <shivkarthi.sundar@gmail.com>
2021-02-13 08:57:50 -0600

    print out cool stuff

--- hackhealth/src/App.js
+++ hackhealth/src/App.js
@@ -1,22 +1,47 @@
 import './App.css';
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import firebase from "firebase/app";
+import "firebase/database";
+const keys = require('./keys.json');
 
-export default function SignIn() {
+// const list1 = [];
+
+function App() {
+  var database = firebase.database();
+  database.ref("nationality/burmese").get().then(function(snapshot) {
+    if (snapshot.exists()) {
+      snapshot.forEach(function(childSnapshot) {
+        // list1.push(childSnapshot.key);
+        console.log(childSnapshot.key);
+      })
+    }
+  });
+  // const numbers = [1, 2, 3, 4, 5];
+  // console.log(numbers);
+  // console.log(list1);
+  // // const listItems = list1.forEach()
+  // const listItems = list1.map((value) =>
+  //   return(
+  //     <li>{value}</li>
+  //   );
+  // );
+
   return (
-    <Router>
-      <div>
-        <Link to="/nationality">Sign In</Link>
-      </div>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
-function App() {
-
-}
+export default App;
