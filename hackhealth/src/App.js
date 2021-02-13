32ca49c4f04d633143901f5a13c603e4b894622c
shiv-sundar <shivkarthi.sundar@gmail.com>
2021-02-13 04:33:53 -0600

    messing around with so much logic at once

--- hackhealth/src/App.js
+++ hackhealth/src/App.js
@@ -1,7 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import firebase from "firebase/app";
+import "firebase/database";
+const keys = require('./keys.json');
 
 function App() {
+  const firebaseConfig = {
+    apiKey: keys.apiKey,
+    authDomain: keys.authDomain,
+    projectId: keys.projectId,
+    storageBucket: keys.storageBucket,
+    messagingSenderId: keys.messagingSenderId,
+    appId: keys.appId
+  };
+  firebase.initializeApp(firebaseConfig);
+  var database = firebase.database();
+  console.log(database.ref("test/password").key);
+  console.log(database.ref("test/username").key);
+  database.ref("test/xx/too_bad").get().then(function(snapshot) {
+    if (snapshot.exists()) {
+      console.log(snapshot.val());
+    }
+  });
+  // database.ref("test/bruh").set({
+  //   username: "bruh",
+  //   password: "bruh2"
+  // });
   return (
     <div className="App">
       <header className="App-header">
