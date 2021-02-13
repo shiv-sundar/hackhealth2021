a018c7dfc3e7c9dbc7d0e5f2065ca58dc70dca42
shiv-sundar <shivkarthi.sundar@gmail.com>
2021-02-13 06:54:56 -0600

    working on routing webpages

--- hackhealth/src/App.js
+++ hackhealth/src/App.js
@@ -1,49 +1,22 @@
 import './App.css';
-import firebase from "firebase/app";
-import "firebase/database";
-const keys = require('./keys.json');
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-function App() {
-  var database = firebase.database();
-  database.ref().set({
-    "nationality": {
-      "burmese": "x",
-      "tibetan": "x",
-      "latvian": "x",
-      "nigerian": "x",
-      "indian": "x",
-      "korean": "x"
-    }
-  });
-  // console.log(new Database().getDatabase().ref("test/password").key);
-  // console.log(database.ref("test/password").key);
-  // console.log(database.ref("test/username").key);
-  // database.ref("test/xx/too_bad").get().then(function(snapshot) {
-  //   if (snapshot.exists()) {
-  //     console.log(snapshot.val());
-  //   }
-  // });
-  // database.ref("test/bruh").set({
-  //   username: "bruh",
-  //   password: "bruh2"
-  // });
+export default function SignIn() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Link to="/nationality">Sign In</Link>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+function App() {
+
+}
